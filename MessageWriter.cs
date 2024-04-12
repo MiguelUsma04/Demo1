d17c71376c0c818f8f1124cf9189c26f2f547337
Miguel Usma <usmamiguel04@gmail.com>
2024-04-12 00:21:14 -0500

    Clases Console y Library

---
+++ MessageWriter.cs
@@ -0,0 +1,12 @@
+
+namespace MyLibrary
+{
+
+	public class MessageWriter
+{
+	public void Writer(string message)
+	{
+		Console.WriteLine(message);
+	}
+}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyConsoleApp
+
+using MyLibrary;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Writer("HELLO WRITER!");
+        }
+    }
+}
